f1e1155614109f434df3e7d4c16ea68e91f359de
a <99947405+GowthamGow11@users.noreply.github.com>
2022-12-01 23:28:59 +0530

    Update prog12.py

--- prog12.py
+++ prog12.py
@@ -1,59 +1,40 @@
 from tkinter import *
-class MyWindow:
-    def __init__(self, win):
-        self.lbl1=Label(win, text='First number')
-        self.lbl2=Label(win, text='Second number')
-        self.lbl3=Label(win, text='Result')
-        self.t1=Entry()
-        self.t2=Entry()
-        self.t3=Entry()
-        self.btn1 = Button(win, text='Add')
-        self.btn2=Button(win, text='Subtract')
-        self.btn3=Button(win,text="Multilication")
-        self.btn4=Button(win,text="Division")
-        self.lbl1.place(x=100, y=50)
-        self.t1.place(x=200, y=50)
-        self.lbl2.place(x=100, y=100)
-        self.t2.place(x=200, y=100)
-        self.b1 = Button(win, text='Add', command=self.add)
-        self.b2 = Button(win, text='Subtract')
-        self.b2.bind('<Button-1>', self.sub)
-        self.b3 = Button(win, text="Multiplication")
-        self.b3.bind('<Button-1>', self.mul)
-        self.b4 = Button(win, text="Divisin")
-        self.b4.bind('<Button-1>', self.div)
-        self.b1.place(x=100, y=150)
-        self.b2.place(x=200, y=150)
-        self.b3.place(x=300, y=150)
-        self.b4.place(x=400, y=150)
-        self.lbl3.place(x=100, y=200)
-        self.t3.place(x=200, y=200)
-    def add(self):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=num1+num2
-        self.t3.insert(END, str(result))
-    def sub(self, event):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=num1-num2
-        self.t3.insert(END, str(result))
-    def mul(self, event):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=num1*num2
-        self.t3.insert(END, str(result))
-    def div(self, event):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=num1/num2
-        self.t3.insert(END, str(result))
-window=Tk()
-mywin=MyWindow(window)
-window.title('Tkinter Arthematic operations')
-window.geometry("700x500+10+10")
-window.mainloop()
+ 
+def add_numbers():
+    res=int(e1.get())+int(e2.get())
+    label_text.set(res)
+def sub_numbers():
+    res=int(e1.get())-int(e2.get())
+    label_text.set(res)
+def mul_numbers():
+    res=int(e1.get())*int(e2.get())
+    label_text.set(res)
+def div_numbers():
+    res=int(e1.get())/int(e2.get())
+    label_text.set(res)
+ 
+window = Tk()
+label_text=StringVar();
+Label(window, text="Enter First Number:").grid(row=0, sticky=W)
+Label(window, text="Enter Second Number:").grid(row=1, sticky=W)
+Label(window, text="Result of Addition:").grid(row=3, sticky=W)
+result=Label(window, text="", textvariable=label_text).grid(row=3,column=1, sticky=W)
+ 
+e1 = Entry(window)
+e2 = Entry(window)
+ 
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+ 
+c = Button(window, text="ADD", command=add_numbers)
+b = Button(window, text="SUB", command=sub_numbers)
+d = Button(window, text="MUL", command=mul_numbers)
+f = Button(window, text="DIV", command=div_numbers)
+
+c.grid(row=0, column=2,columnspan=2, rowspan=1,sticky=W+E+N+S, padx=5, pady=5)
+b.grid(row=1, column=2,columnspan=2, rowspan=1,sticky=W+E+N+S, padx=5, pady=5)
+d.grid(row=2, column=2,columnspan=2, rowspan=1,sticky=W+E+N+S, padx=5, pady=5)
+f.grid(row=3, column=2,columnspan=2, rowspan=1,sticky=W+E+N+S, padx=5, pady=5)
+
+
+mainloop()
